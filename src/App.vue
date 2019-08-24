b32e9a461b1d8603f5929044e1d5624b36bda664
Mr.yao <lily@dehuinet.com>
2019-08-24 16:52:21 +0800

    .

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+      hello world
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
